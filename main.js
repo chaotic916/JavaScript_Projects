c6b8dd92874f897d996a190621b7b6132b292a4f
chaotic916 <daehee.lee916@gmail.com>
2023-12-22 16:58:19 +0800

    initial commit

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log('Fuck Korea')
+const { crawlPage } = require('./crawl');
+
+
+// The entry point of the application
+function main() {
+  if (process.argv.length < 3) {
+    console.log("No website provided for the action")
+    process.exit(1)
+  }
+  if (process.argv.length > 3) {
+    console.log("Too many command line arguments")
+    process.exit(1)
+  }
+
+  // process.argv[2] and onwards are the command-line arguments provided by the user
+  const baseURL = process.argv[2]
+
+  console.log(`Crawling initiated: ${baseURL}`)
+  crawlPage(baseURL);
+};
+
+main();
